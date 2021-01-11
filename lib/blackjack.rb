35e0f3d98a418dfde750d29a685f773c18554388
calm-collection-3916 <khalinredd@gmail.com>
2021-01-11 02:52:59 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -43,6 +43,11 @@ def hit?
     invalid_command
     prompt_user
     user_input = get_user_input
+    end
+  if user_input == "h"
+    current_card_value += deal_card
+  end
+  return current_card_value
 end
 
 def invalid_command
